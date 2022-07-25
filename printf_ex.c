98ad543c68f736e5f1ef72008ab477763123e03b
woojeong <woojeong@c5r7s1.42seoul.kr>
2022-07-25 20:01:53 +0900

    print

--- printf_ex.c
+++ printf_ex.c
@@ -55,12 +55,19 @@ int    check_width(const char **p_str)
     return (change_num(p_str));
 }
 
-int     check_precision(const char **p_str)
+int     check_precision(const char **p_str, int *opt)
 {
+    //'.'이 찍혀있으면 일단 1을 opt[pre]에 넣어서 pre가 있음을 표시
+    int exist_pre;
+
+    exist_pre = 0;
     if (**p_str == '.')
     {
+        exist_pre = change_num(p_str);
+        if (exist_pre == 0)
+            exist_pre = 1;
         (*p_str)++;
-        return (change_num(p_str));
+        return (exist_pre);
     }
     else
         return (0);
@@ -100,18 +107,92 @@ void    parse(const char **p_str, int *opt)
 //     printf("type : %c", opt[type]);
 }
 
+void    put_space(int len, int is_zero)
+{
+    char    space;
+
+    if (is_zero)
+        space = '0';
+    else
+        space = ' ';
+    while (len-- > 0)
+        write(1, &space, 1);
+}
+
+int len_int(int num)
+{
+    int len;
+
+    len = 1;
+    while (num / 10)
+    {
+        len++;
+        num /= 10;
+    }
+    return (len);
+}
+
+void    put_int(int num, int *opt, int num_len, int word_len)
+{
+    int digit;
+
+    if (num < 0)
+        write(1, "-", 1);
+    else if (opt[space] == 1)
+        write(1, " ", 1);
+    else if (opt[plus] == 1)
+        write(1, "+", 1);
+    else
+        word_len++;
+    put_space(word_len - num_len - 1, 1);
+    ft_putnbr(num);        
+}
+
+void    print_int(int num, int *opt)
+{
+    int word_len;
+    int num_len;
+    
+    num_len = len_int(num); //순수 숫자의 길이
+    word_len = num_len; 
+    if (num_len < opt[presicion])
+        word_len = opt[presicion]; //정확도 추가
+    if (num < 0 || opt[plus] == 1 || opt[space] == 1)
+        word_len++;  //부호 추가
+    if (opt[presicion] == 0 && opt[zero] == 1 && opt[width] > word_len) //정확도가 없는데 flag'0'가 사용되면
+    {
+        num_len += opt[width] - word_len;
+        word_len = opt[width];
+    }
+    if (!opt[minus])
+    {
+        put_space(opt[width] - word_len, opt[zero] * (!opt[presicion]));
+        put_int(num, opt, num_len, word_len);
+    }
+    else
+    {
+        put_int(num, opt, num_len, word_len);
+        put_space(width - word_len, 0);
+    }
+}
+
 void    convert_print(int *opt, va_list *ap, int *len)
 {
-    // if (opt[type] == 'c' || opt[type] == 'd' || opt[type] == 'i' )
-    //     print_int(va_arg(*ap, int), opt);
-    // else if (opt[type] == 'u'|| opt[type] == 'x' || opt[type] == 'X' )
+    
+    if (opt[type] == 'd' || opt[type] == 'i' )
+        print_int(va_arg(*ap, int), opt);
+    // else if (opt[type] == 'c')
+    //     print_char(va_arg(*ap, int), opt);
+    // else if (opt[type] == 'u')
     //     print_unsigned_int(va_arg(*ap, unsigned int), opt);
+    // else if (opt[type] == 'x' || opt[type] == 'X' )
+    //     print_sixteen(va_arg(*ap, unsigned int), opt);
     // else if (opt[type] == 'p')
-    //     print_unsigend_long(va_arg(*ap, unsigned long), opt);
-    // else if (opt[type] == '%')
-    //     printf("%%");//보류
+    //     print_adress(va_arg(*ap, unsigned long), opt);
     // else if (opt[type] == 's')
     //     print_string(va_arg(*ap, char *), opt);
+    // else if (opt[type] == '%')
+    //     write(1, "%%", 1);
 }
 
 void init_option(int *opt)
@@ -162,8 +243,65 @@ int ft_printf(const char *str, ...)
     return (len);
 }
 
+
+
+/*  flag_oder
+10진수(%d, %i)
+        무시됨(에러)        같이 적용됨       무시하고 적용    
+' ' :     '+'(사용불가)     '-','0'          
+'0' :     '-'(사용불가)     ' ','+'
+'+' :     ' '(사용불가)     '-','0'
+'-' :     '0'(사용불가)     ' ','+'
+
+unsigned 10진수(%u)
+            사용불가           사용가능(중복불가)
+        ' ','+'(부호관련)       '-','0'
+
+주소(%p)
+        사용불가           사용가능(중복불가)
+      ' ','+','0','#'            '-'
+
+캐릭터(%c)
+        사용불가           사용가능(중복불가)
+      ' ','+','0'            '-'
+
+문자열(%s)
+        사용불가           사용가능(중복불가)
+      ' ','+','0'            '-'
+
+16진수(%x, %X)
+            사용불가        사용가능(중복불가)
+'#'쓸때 :    '+',' '        '-','0'                     
+'#'안쓸때 :   '+',' '        '-','0'
+
+문자'%' (%%)
+    '%' 쓸데는 에러없이 플래그 전부 무시되고 '%'출력
+*/
+
+
 int main()
 {
-    ft_printf("num1 : %+-0 #13.7d\n", 5);
-    printf("[%0.5x]", 16*16-10);
+    //ft_printf("num1 : %+-0 #13.7d\n", 5);
+    int num = 1;
+    printf("[%-010x]", 16*16-10);
 }
+/*width_precision
+
+width : 부호와 공백 포함 전체 길이
+precition : 부호와 공백을 제외하고 0과 숫자까지의 길이ㅣ
+
+(wid == 0) && (pre > 0)
+    부호제외 0과 숫자의 길이가 pre
+(wid > 0) && (pre == 0)
+    공백 부호 숫자 모두합이 wid
+(wid == 0) && (pre == 0)
+    공백없이 부호 숫자만 알맞게 출력
+(wid > 0) && (pre > 0)
+    우선적으로 pre만큼 숫자와 0을 찍고
+    부호를 찍고
+    wid가 남으면 공백을 찍는다
+
+####precision이 적용될때 '0'flag는 무시되고 pre를 초과하는 공백은 무조건 ' '으로 표시
+*/
+
+//출력시 공백 부호 숫자 세가지로
--- printf_util.c
+++ printf_util.c
@@ -1,3 +1,20 @@
+unsigned long   ft_exp(int n, int p)
+{
+    int i;
+    unsigned long   rst;
+
+    i = 0;
+    rst = 1;
+    if (p == 0)
+        reutrn (1);
+    while (i < p)
+    {
+        rst *= n;
+        i++;
+    }
+    return (rst);
+}
+
 void    put_space(int len, int is_zero)
 {
     char    space;
@@ -44,15 +61,20 @@ void    print_int(int num, int *opt)
     int word_len;
     int num_len;
     
-    num_len = len_int(num);
-    word_len = num_len;
+    num_len = len_int(num); //순수 숫자의 길이
+    word_len = num_len; 
     if (num_len < opt[presicion])
-        word_len = opt[presicion];
+        word_len = opt[presicion]; //정확도 추가
     if (num < 0 || opt[plus] == 1 || opt[space] == 1)
-        word_len++;
+        word_len++;  //부호 추가
+    if (opt[presicion] == 0 && opt[zero] == 1 && opt[width] > word_len) //정확도가 없는데 flag'0'가 사용되면
+    {
+        num_len += opt[width] - word_len;
+        word_len = opt[width];
+    }
     if (!opt[minus])
     {
-        put_space(width - word_len, opt[zero] * (!opt[presicion]));
+        put_space(opt[width] - word_len, opt[zero] * (!opt[presicion]));
         put_int(num, opt, num_len, word_len);
     }
     else
@@ -79,20 +101,55 @@ void    print_char(char ch, int *opt)
     }
 }
 
+int len_ul(unsigned long num)
+{
+    int len;
+
+    len = 1;
+    while (num / 10)
+    {
+        len++;
+        num /= 10;
+    }
+    return (len);
+}
+
+void    put_uint(unsigned int num, int word_len, int num_len);
+{
+    char    ch;
+    unsigned int div;
+
+    put_space(word_len - num_len, 1);
+    if (num == 0)
+        write(1, "0", 1);
+    else
+    {
+        div = (unsigned int)ft_exp(10, num_len);
+        while (div >= 1)
+        {
+            ch = (num / div) + '\0';
+            write(1, &ch, 1);
+            div /= 10;
+        }
+    }   
+}
+
 void    print_unsigned_int(unsigned int num, int *opt)
 {
     int word_len;
-    
+    int num_len;
 
-    word_len = len_ul((unsigned long)num, opt);
+    num_len = len_ul((unsigned long)num);
+    if (num_len < opt[presicion])
+        word_len = opt[presicion];
     if (!opt[minus])
     {
-        put_space(width - word_len, opt[zero]);
-        put_uint();
+        put_space(width - word_len, opt[zero] * !opt[presicion]);
+        put_uint(num, word_len, num_len);
     }
     else
     {
-        put_uint();
+        put_uint(num, word_len, num_len);
         put_space(width - word_len, 0);
     }
 }
@@ -100,12 +157,16 @@ void    print_unsigned_int(unsigned int num, int *opt)
 void    print_sixteen(unsigned int hexa, int *opt)
 {
     int word_len;
-    
+    int num_len;
 
-    word_len = len_ul((unsigned long)hexa, opt);
+    num_len = len_ul((unsigned long)hexa);
+    if (num_len < opt[presicion])
+        word_len = opt[presicion];
+    if (opt[shop] == 1)
+        word_len += 2;
     if (!opt[minus])
     {
-        put_space(width - word_len, opt[zero]);
+        put_space(width - word_len, 0);
         put_hexa(hexa, opt[shop]);
     }
     else
@@ -118,9 +179,9 @@ void    print_sixteen(unsigned int hexa, int *opt)
 void    print_adress(unsigned long addr, int *opt)
 {
     int word_len;
-    
+    int num_len;
 
-    word_len = len_ul(addr, opt);
+    num_len = len_ul(addr);
     if (!opt[minus])
     {
         put_space(width - word_len, opt[zero]);
