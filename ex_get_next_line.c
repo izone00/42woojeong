34329aca2ffb0469e5ab06ee6a7a337cfaa453d6
woojeong <woojeong@c4r4s3.42seoul.kr>
2022-07-29 17:20:56 +0900

    GNL finish

---
+++ ex_get_next_line.c
@@ -0,0 +1,183 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ex_get_next_line.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: woojeong <woojeong@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/27 20:01:12 by woojeong          #+#    #+#             */
+/*   Updated: 2022/07/29 13:34:35 by woojeong         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ex_get_next_line.h"
+
+char    *get_next_line(int fd)  
+{
+    static      t_fd_list *remain_box;//이전에 남은 remain들의 box 자동으로 '0'초기화
+    // char        remain_str[BUFF_SIZE + 1];//해당 fd에서 가져온 remain을 담은 str
+    t_read_list read_list;
+    //char        *line;
+    int         line_len;
+
+    //사용할 remain이 있는지, 새로운 fd_box를 만들지, 원하는 만큼 from_remain에 복사 해둔다, remain_box를 수정한다.
+    // printf("[box addr : %p]\n",remain_box);
+    (read_list.str)[0] = '\0';
+    read_list.next = NULL;
+    line_len = check_fd_remain_box(&remain_box, read_list.str, fd);
+    if (line_len == (-1))
+    {
+        // printf("[mem_err]\n");
+        return (NULL);
+    }
+
+    //실행후에는 remain_box에서 정보를 사용하고 수정되어있고 from_remain에는 사용할 문자열이 담긴다.
+    if (line_len == 0 || (read_list.str)[line_len - 1] != '\n')//엔터키가 없어 더 읽어야한다면
+    {
+        if (read_file_until_nl(fd, &line_len, &remain_box, &read_list) == (-1))
+            return (NULL);
+    }
+    printf("[line_len : %d]\n", line_len);
+    //위 함수들이 실행된 이후에는 최종 line의 길이, 이전 문자열, 새로 읽은 문자열 리스트가 설정되고 읽다가 남은 문자열을 box에 저장한다.
+    return (make_new_line(line_len, &read_list));
+}
+
+int read_file_until_nl(int fd, int *line_len, t_fd_list **remain_box, t_read_list *read_list)
+{
+    char    buff[BUFF_SIZE];
+    int         read_len;
+    t_fd_list *cur_fd_box;
+    t_read_list *cur_read;
+
+    cur_read = read_list;
+    // printf("[after box addr : %p]\n", *remain_box);
+    cur_fd_box = *remain_box;
+    cur_fd_box = search_fd_in_box(&cur_fd_box, fd);
+    // printf("[after box addr : %p]\n", *remain_box);
+    // printf("[cur fd : %d]\n", cur_fd_box -> fd);
+    while (0 < (read_len = read(fd, buff, BUFF_SIZE)))
+    {
+        *line_len += read_len;
+        // printf("[line : %d read : %d]\n", *line_len, read_len);
+        cur_read -> next = (t_read_list *)malloc(sizeof(t_read_list));
+        cur_read = cur_read -> next;
+        if (cur_read == NULL)
+            return (read_free(read_list -> next));
+        (cur_read -> str)[0] = '\0';
+        cur_read -> next  = NULL;
+        if (cpy_until_nl(cur_read -> str, buff, cur_fd_box -> str, read_len))
+        {
+            // printf("[remain : (%s), read : (%s)]\n", cur_fd_box -> str, cur_read -> str);
+            return (1);//nl이 있었으면 1을 반환
+        }
+    }
+    printf("[read_len : %d fd : %d]\n", read_len, fd);
+    fd_free(fd, remain_box);// 밖으로 빼야할듯? ㄴㄴ
+    return (1);
+}
+
+char    *make_new_line(int line_len, t_read_list *read_list)
+{
+    t_read_list *cur_read;
+    int         idx;
+    int         jdx;
+    char        *line;
+
+    if (line_len == 0)
+        return (NULL);
+    if ((line = (char *)malloc(line_len + 1)) == NULL)
+    {
+        read_free(read_list -> next);
+        return (NULL);
+    }
+    line[0] = '\0';
+    cur_read = read_list;
+    idx = 0;
+    while (cur_read)
+    {
+        jdx = 0;
+        while ((cur_read -> str)[jdx])
+            line[idx++] = (cur_read -> str)[jdx++];
+        line[idx] = '\0';
+        cur_read = cur_read -> next;
+    }
+    read_free(read_list -> next);// cpy함수 안에서 해도됨
+    return (line);
+}
+
+int cpy_until_nl(char *str, char *buff, char *remain_str, int read_len)
+{
+    int idx;
+    int jdx;
+
+    idx = 0;
+    while (idx < read_len)
+    {
+        str[idx] = buff[idx];
+        if (str[idx] == '\n')
+        {
+            jdx = 0;
+            idx++;
+            str[idx] = '\0';
+            while (idx < read_len)
+            {
+                remain_str[jdx] = buff[idx];
+                idx++;
+                jdx++;
+            }
+            remain_str[jdx] = '\0';
+            return (1);
+        }
+        idx++;
+    }
+    str[idx] = '\0';
+    return (0);
+}
+
+int read_free(t_read_list *list)
+{
+    t_read_list *pre;
+
+    while (list)
+    {
+        pre = list;
+        list = list -> next;
+        free(pre);        
+    }
+    return (-1);
+}
+
+
+#include <fcntl.h>
+int main()
+{
+    int fd1;
+    int fd2;
+    char *str;
+    int i = 1;
+
+    fd1 = open("./test.txt", O_RDONLY);
+    fd2 = open("./test2.txt", O_RDONLY);
+    int fd3= open("./test3.txt", O_RDONLY);
+    if ((str = get_next_line(10)) == NULL)
+        printf("is NULL\n");
+    while (i <= 8)
+    {
+        str = get_next_line(fd1);
+        if (!str)
+            break;
+        printf("(fd1) %d: %s/\n", i, str);
+        //free(str);
+        str = get_next_line(fd2);
+        if (!str)
+            break;
+        printf("(fd2) %d: %s/\n", i, str);
+        //free(str);
+        str = get_next_line(fd3);
+        if (!str)
+            break;
+        printf("(fd3) %d: %s/\n", i, str);
+        //free(str);
+        i++;
+    }
+}
---
+++ ex_get_next_line_utils.c
@@ -0,0 +1,123 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ex_get_next_line_utils.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: woojeong <woojeong@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/28 18:53:04 by woojeong          #+#    #+#             */
+/*   Updated: 2022/07/29 16:55:08 by woojeong         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ex_get_next_line.h"
+
+int check_fd_remain_box(t_fd_list **remain_box, char *remain_str,int fd)
+{
+    t_fd_list   *box;
+    int         remain_len;
+
+    if (*remain_box == NULL)
+    {
+        // printf("[remain_box is NULL]\n");
+        if (new_fd_box(fd, remain_box) == (-1))//(-1)은 mem_err
+            return (-1);
+        box = *remain_box;
+        // printf("[fd : %d, next : %d]\n", box -> fd, (box -> next) == NULL);
+    }
+    else
+    {
+        // printf("[remain_box is NEW]\n");
+        box = *remain_box;
+        if(!search_fd_in_box(&box, fd))
+        {
+            // printf("[new fd : %d]\n", fd);
+            if (new_fd_box(fd, &(box -> next)) == (-1))
+                return (-1);
+            box = box -> next;
+        }
+    }
+    remain_len = pull_from_box(remain_str, box);
+    // printf("[remain_str : (%s)]\n", remain_str);
+    return (remain_len);
+}
+//remain_str에 복사해주고 box도 수정해준다
+int pull_from_box(char *remain_str, t_fd_list *box)
+{
+    int len;
+    int dest;
+
+    len = 0;
+    while ((box -> str)[len])
+    {
+        remain_str[len] = (box -> str)[len];
+        if (remain_str[len] == '\n')
+        {
+            len++;
+            break;
+        }
+        len++;
+    }
+    remain_str[len] ='\0';
+    dest = 0;
+    while ((box -> str)[len + dest])
+    {
+        (box -> str)[dest] = (box -> str)[len + dest];
+        dest++;
+    }
+    (box -> str)[dest] = '\0';
+    return (len);
+}
+
+int new_fd_box(int fd, t_fd_list **box)
+{
+    *box = (t_fd_list *)malloc(sizeof(t_fd_list));
+    if (*box == NULL)
+        return (-1);
+    (*box) -> next = NULL;
+    (*box) -> fd = fd;
+    ((*box) -> str)[0] = '\0';
+    return (1);
+}
+
+t_fd_list   *search_fd_in_box(t_fd_list **box, int fd)
+{
+    while ((*box) -> next != NULL)
+    {
+        if ((*box) -> fd == fd)
+            return (*box);
+        (*box) = (*box) -> next;
+    }
+    if ((*box) -> fd == fd)
+        return (*box);
+    else
+        return (0);
+}
+
+void    fd_free(int fd, t_fd_list **remain_box)
+{
+    t_fd_list *pre;
+    t_fd_list *cur;
+
+    cur = *remain_box;
+    printf("[cur -> fd : %d fd : %d\n", cur -> fd, fd);
+    if (cur -> fd == fd)
+    {
+        *remain_box = (*remain_box) -> next;
+        free(cur);
+    }
+    else
+    {
+        while (cur)
+        {
+            pre = cur;
+            cur = cur -> next;
+            if (cur -> fd == fd)
+            {
+                pre -> next = cur -> next;
+                free(cur);
+                return;
+            }
+        }
+    }
+}
