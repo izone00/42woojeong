f7a6e3d154187cd03e035a9cb947aa06dc7dccaf
woojeong <woojeong@c2r10s2.42seoul.kr>
2022-08-17 18:17:32 +0900

    6/17 cluster

--- pipex_ex/main.c
+++ pipex_ex/main.c
@@ -15,27 +15,28 @@ int main(int argc, char *argv[])
 {
 	pid_t	pid;
 	char	filename1[20] = "/bin/";
-	char	filename2[20] = "/bin/";
+	char	filename2[20] = "/usr/bin/";
 	char	**command1;
 	char	**command2;
-	int		fd1;
 	char	buff[11];
 	int		read_len;
 	int		stat;
+	int		fd[2];
+	int		fd1;
 
-	command1 = ft_split(argv[2], ' ');
-	strcat(filename1, command1[0]);
-
-	fd1 = open(argv[1], O_RDONLY);//argv[1]는 정확한 경로를 전달해 준다.
-	int	fd_out;
-	fd_out = dup(1); //fd_out = stdout
-	dup2(0, 1);//1 -> stdin
-	dup2(fd1, 0);// 0 -> fd1
+	pipe(fd);
 	pid = fork();
 	if (pid == 0)
+	{
+		fd1 = open(argv[1], O_RDONLY);//argv[1]는 정확한 경로를 전달해 준다.
+		command1 = ft_split(argv[2], ' ');
+		strcat(filename1, command1[0]);
+		dup2(fd1, 0);// 0 -> fd1
+		dup2(fd[1], 1);
 		execve(filename1, command1, environ);
+	}
 	wait(&stat);
-	dup2(fd_out, 1);// 1 -> stdout
+	dup2(fd[0], 0);
 	command2 = ft_split(argv[3], ' ');
 	strcat(filename2, command2[0]);
 	execve(filename2, command2, environ);
