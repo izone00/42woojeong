d92460a39ab5d3008092d6612e65acae185e0490
woojin <91773764+izone00@users.noreply.github.com>
2022-08-17 11:11:16 +0900

    pipe

--- pipex_ex/main.c
+++ pipex_ex/main.c
@@ -13,17 +13,31 @@ extern char	**environ;
 
 int main(int argc, char *argv[])
 {
-	char	filename[20] = "/bin/";
-	char	**command;
+	pid_t	pid;
+	char	filename1[20] = "/bin/";
+	char	filename2[20] = "/bin/";
+	char	**command1;
+	char	**command2;
 	int		fd1;
 	char	buff[11];
 	int		read_len;
+	int		stat;
 
-	command = ft_split(argv[2], ' ');
-	strcat(filename, command[0]);
+	command1 = ft_split(argv[2], ' ');
+	strcat(filename1, command1[0]);
 
 	fd1 = open(argv[1], O_RDONLY);//argv[1]는 정확한 경로를 전달해 준다.
-	dup2(fd1, 0);
-	execve(filename, command, environ);
+	int	fd_out;
+	fd_out = dup(1); //fd_out = stdout
+	dup2(0, 1);//1 -> stdin
+	dup2(fd1, 0);// 0 -> fd1
+	pid = fork();
+	if (pid == 0)
+		execve(filename1, command1, environ);
+	wait(&stat);
+	dup2(fd_out, 1);// 1 -> stdout
+	command2 = ft_split(argv[3], ' ');
+	strcat(filename2, command2[0]);
+	execve(filename2, command2, environ);
 	perror("err");
 }
