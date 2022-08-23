23e05d202597b26def3cc39ddcec72a867707869
woojeong <woojeong@c1r7s5.42seoul.kr>
2022-08-23 19:55:41 +0900

    cl 8/23

--- pipex_ex/ex.c
+++ pipex_ex/ex.c
@@ -1,10 +1,58 @@
-#include <stdio.h>
+#include <stdio.h>//perror
+#include <string.h>//strerror
+#include <unistd.h>//access
 
-int main(int argc, char *argv[])
+#include <stdlib.h>
+#include <errno.h>
+#include <fcntl.h>
+
+#define BUFF_SIZE 3
+
+int	main()
 {
-	for (int i = 0; i < argc; i++)
+	char buff[11];
+	int fd1 = open("./in1", O_RDONLY);
+	int fd2 = 10;
+	int fd3 = 20;
+	int read_len;
+	int pid;
+	int stat;
+	int pipefd[2];
+
+	pipe(pipefd);
+	pid = fork();
+	if (pid != 0)
+		printf("[1. %d %d]\n", pipefd[0], pipefd[1]);
+	if (pid == 0)
 	{
-		printf("[%d : %s]\n", i, argv[i]);
+		write(pipefd[1], "pipe\n", 5);
+		// close(pipefd[1]);
 	}
-	return (0);
+	else
+	{
+		wait(&stat);
+		dup2(pipefd[0], fd2);
+		close(pipefd[0]);
+		read_len = read(fd2, buff, BUFF_SIZE);
+		write(1, buff, read_len);
+		// dup2(pipefd[1], fd3);
+		close(pipefd[1]);
+		close(fd2);
+		// close(fd3);
+		int pip = pipe(pipefd);
+		if ((-1) == write(pipefd[1], "second\n", 6))
+			write(1, "write fail\n", 11);
+		read_len = read(pipefd[0], buff, BUFF_SIZE);
+		printf("[read len : %d]\n", read_len);
+		write(1, buff, read_len);
+		printf("[2. %d %d]\n", pipefd[0], pipefd[1]);
+		printf("[%d]\n", pip);
+	}
+	// dup2(fd1, fd2);
+	// close(fd1);
+	// read_len = read(fd2, buff, BUFF_SIZE);
+	// buff[read_len] = '\0';
+	// write(1, buff, read_len);
+
+
 }
--- pipex_ex/main.c
+++ pipex_ex/main.c
@@ -6,39 +6,95 @@
 #include <errno.h>
 #include <fcntl.h>
 char	**ft_split(char const *s, char c);
+int	ft_strncmp(const char *s1, const char *s2, size_t n);
+int	ft_strlen(const char *str);
 
-// extern int errno;
-extern char	**environ;
 #define BUFF_SIZE 10
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[], char *envp[])//env는 null terminate되있다.
 {
+	int		read_len;
+	char	buff[11];
+
 	pid_t	pid;
-	char	filename1[20] = "/bin/";
-	char	filename2[20] = "/usr/bin/";
 	char	**command1;
 	char	**command2;
-	char	buff[11];
-	int		read_len;
+	char	**filepath;
+	char	filename1[30];
+	char	filename2[30];
 	int		stat;
-	int		fd[2];
+	int		pipefd[2];
 	int		fd1;
+	int		fd2;
+	int		idx;
 
-	pipe(fd);
+	idx = 0;
+	filepath = NULL;
+	while (envp[idx])
+	{
+		if (!ft_strncmp("PATH=", envp[idx], 5))
+			filepath = ft_split(envp[idx], ':');
+		idx++;
+	}
+	if (!filepath)
+		perror("no PATH"); // PATH경로가 없을때
+
+	if ((-1) = pipe(pipefd))
+		perror("pipe");
 	pid = fork();
 	if (pid == 0)
 	{
-		fd1 = open(argv[1], O_RDONLY);//argv[1]는 정확한 경로를 전달해 준다.
+		if ((-1) == (fd1 = open(argv[1], O_RDONLY)))//argv[1]는 정확한 경로를 전달해 준다.
+			perror("zsh");
 		command1 = ft_split(argv[2], ' ');
-		strcat(filename1, command1[0]);
+		idx = 0;
+		while (filepath[idx])
+		{
+			strcpy(filename1, filepath[idx]);
+			strcat(filename1, "/");
+			strcat(filename1, command1[0]);
+			if (!access(filename1, F_OK))
+				break;
+			idx++;
+		}
+		if (!filepath[idx])
+		{
+			write(2, "zsh: command not found: ", 24);
+			write(2, command1[0], ft_strlen(command1[0]));
+			exit(127);
+		}
 		dup2(fd1, 0);// 0 -> fd1
-		dup2(fd[1], 1);
-		execve(filename1, command1, environ);
+		dup2(pipefd[1], 1);
+		execve(filename1, command1, envp);
 	}
-	wait(&stat);
-	dup2(fd[0], 0);
-	command2 = ft_split(argv[3], ' ');
-	strcat(filename2, command2[0]);
-	execve(filename2, command2, environ);
-	perror("err");
+	else
+	{
+		wait(&stat);
+		if 
+		dup2(pipefd[0], 0);
+		close(pipefd[1]);
+		command2 = ft_split(argv[3], ' ');
+		idx = 0;
+		while (filepath[idx])
+		{
+			strcpy(filename2, filepath[idx]);
+			strcat(filename2, "/");
+			strcat(filename2, command2[0]);
+			if (!access(filename2, X_OK))
+				break;
+			idx++;
+		}
+		if (!filepath[idx])
+		{
+			write(2, "zsh: command not found: ", 24);
+			write(2, command2[0], ft_strlen(command2[0]));
+			exit(127);
+		}
+		char *outfile = malloc(sizeof(char) * (strlen(argv[argc - 1]) + 3));
+		strcpy(outfile, "./");
+		strcat(outfile, argv[argc - 1]);
+		int fd_out = open(outfile, (O_RDWR | O_CREAT), S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+		dup2(fd_out, 1);
+		execve(filename2, command2, envp);
+	}
 }
